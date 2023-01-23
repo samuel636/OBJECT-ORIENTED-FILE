56677f0a59a8ee4f422ea8859abd11e5088b322b
samuel636 <117814218+samuel636@users.noreply.github.com>
2023-01-23 14:50:06 +0200

    created a method

--- FILE1.java
+++ FILE1.java
@@ -15,3 +15,22 @@ class CodeLock
 	public boolean isOpen;
 	
 }
+
+public CodeLock()
+	{
+	    isOpen= false;
+	    
+	
+		  code = "1234";
+	}
+	public void printStatus()
+	{
+	 if(true==isOpen)
+	 
+		 System.out.println("The door is open");
+	 
+	 else if(false==isOpen) 
+	 
+		 System.out.println("The door is closed");
+	 
+	}
