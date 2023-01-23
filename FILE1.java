34c91fe5b67711112081ede63f5e0053462714cd
samuel636 <117814218+samuel636@users.noreply.github.com>
2023-01-23 14:45:06 +0200

    created the main class and Code class

---
+++ FILE1.java
@@ -0,0 +1,17 @@
+public class Try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CodeLock codeLock = new CodeLock();
+		codeLock.checkCode("123");	   
+	     
+	     
+	}
+
+}
+class CodeLock
+{    //attribute 
+	public String code;
+	public boolean isOpen;
+	
+}
